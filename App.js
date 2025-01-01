849154e5991b77c1c04c44b5522be31efc4c18ac
Saulo Coelho <coelho.saulo@gmail.com>
2025-01-01 20:14:29 -0300

    feat:install dependencies and add src/

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import 'react-native-gesture-handler';
+import React from 'react';
+import { useFonts, Poppins_400Regular } from '@expo-google-fonts/poppins';
+import Home from './src/paginas/Home';
 export default function App() {
+  let [fontsLoaded, fontError] = useFonts({
+    "PoppinsRegular": Poppins_400Regular,
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Home />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
