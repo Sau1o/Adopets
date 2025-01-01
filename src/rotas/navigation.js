22a7ace58525a1a71a6d689a5a118affcff62eb4
Saulo Coelho <coelho.saulo@gmail.com>
2025-01-01 20:29:17 -0300

    feat: add initial structure for NavigationContainer

---
+++ src/rotas/navigation.js
@@ -0,0 +1,12 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { Text } from "react-native";
+
+const Navigation = () => {
+    return (
+        <NavigationContainer>
+            <Text>Tela de Navigation Conteiner</Text>
+        </NavigationContainer>
+    )
+}
+
+export default Navigation
