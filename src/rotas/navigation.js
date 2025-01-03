b7aab796c8e7fc515427aa39776c895164825ff5
Saulo Coelho <coelho.saulo@gmail.com>
2025-01-02 21:25:01 -0300

    feat(navigation): add native stack navigation using createNativeStackNavigation

--- src/rotas/navigation.js
+++ src/rotas/navigation.js
@@ -1,11 +1,17 @@
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
-import { Text } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from '../paginas/Home';
+import Login from '../paginas/Login';
+
+const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
     <NavigationContainer>
-        <Text>texto</Text>
+        <Stack.Navigator>
+            <Stack.Screen name='Home' component={Home}/>
+            <Stack.Screen name='Login' component={Login}/>
+        </Stack.Navigator>
     </NavigationContainer>
   );
 };
