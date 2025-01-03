9a08da751298c855e318b8006561531b5f27be46
Saulo Coelho <coelho.saulo@gmail.com>
2025-01-03 07:13:47 -0300

    added ScreenOptions

--- src/rotas/navigation.js
+++ src/rotas/navigation.js
@@ -8,8 +8,13 @@ const Stack = createNativeStackNavigator();
 const Navigation = () => {
   return (
     <NavigationContainer>
-        <Stack.Navigator>
-            <Stack.Screen name='Home' component={Home}/>
+        <Stack.Navigator initialRouteName='Home'
+        screenOptions={{
+            headerTitle:'',
+            headerShown:false
+        }}>
+            <Stack.Screen name='Home' 
+            component={Home}/>
             <Stack.Screen name='Login' component={Login}/>
         </Stack.Navigator>
     </NavigationContainer>
